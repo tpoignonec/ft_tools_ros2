75f537396f50a9769e3867d3c84d20610cc10753
Thibault Poignonec <thibault.poignonec@gmail.com>
2023-10-18 22:27:34 +0200

    update examples

--- ft_tools_examples/launch/launch_ft_estimation.launch.py
+++ ft_tools_examples/launch/launch_ft_estimation.launch.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument  # ,IncludeLaunchDescription
 # from launch.conditions import IfCondition, UnlessCondition
 # from launch.event_handlers import OnProcessExit, OnProcessStart
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -47,25 +47,10 @@ def generate_launch_description():
             description='Start robot with fake hardware mirroring command to its states.',
         )
     )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'robot_ip',
-            default_value='192.170.10.2',
-            description='Robot IP of FRI interface',
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'robot_port',
-            default_value='30200',
-            description='Robot port of FRI interface.',
-        )
-    )
+    
     description_package = LaunchConfiguration('description_package')
     description_file = LaunchConfiguration('description_file')
     use_fake_hardware = LaunchConfiguration('use_fake_hardware')
-    robot_ip = LaunchConfiguration('robot_ip')
-    robot_port = LaunchConfiguration('robot_port')
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -82,50 +67,29 @@ def generate_launch_description():
             ' ',
             'use_fake_hardware:=', use_fake_hardware,
             ' ',
-            'robot_ip:=', robot_ip,
-            ' ',
-            'robot_port:=', robot_port,
-            ' ',
             'description_package:=', description_package,
         ]
     )
     robot_description = {'robot_description': robot_description_content}
 
-    # Launch iiwa robot with Moveit2 planning enabled
-    iiwa_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            FindPackageShare('iiwa_bringup'),
-            '/launch',
-            '/iiwa.launch.py'
-        ]),
-        launch_arguments={
-            'description_package': description_package,
-            'description_file': description_file,
-            'use_fake_hardware': use_fake_hardware,
-            'robot_ip': robot_ip,
-            'robot_port': robot_port,
-            'use_planning': 'true',
-        }.items()
-    )
-
-    ft_calibration_node_config = PathJoinSubstitution(
+    # Launch calibration node
+    ft_estimation_node_config = PathJoinSubstitution(
         [
             FindPackageShare('ft_tools'),
             'config',
-            'iiwa_controllers.yaml',
+            'config_ft_estimation.yaml',
         ]
     )
-    ft_calibration_node = Node(
+    ft_estimation_node = Node(
         package='ft_tools',
-        executable='ft_calibration_node',
+        executable='ft_estimation_node',
         namespace='',
-        parameters=[robot_description, ft_calibration_node_config],
+        parameters=[robot_description, ft_estimation_node_config],
         output='both',
     )
 
     nodes = [
-        iiwa_launch,
-        ft_calibration_node
+        ft_estimation_node,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
