0b25b7d52803df091b257cafeac6ac313bea2993
Thibault Poignonec <thibault.poignonec@gmail.com>
2023-10-18 11:23:28 +0200

    add calibration node + config

--- ft_tools/include/ft_tools/ft_calibration_node.hpp
+++ ft_tools/include/ft_tools/ft_calibration_node.hpp
@@ -24,24 +24,36 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include "std_srvs/srv/trigger.hpp"
+#include "kinematics_interface/kinematics_interface.hpp"
+#include "pluginlib/class_loader.hpp"
+
+#include <geometry_msgs/msg/wrench_stamped.hpp>
 
 #include "ft_tools/ft_calibration.hpp"
+#include "ft_tools/joint_state_monitor.hpp"
 #include "ft_msgs/srv/add_calibration_sample.hpp"
 #include "ft_msgs/srv/get_calibration.hpp"
+
+// include generated parameter library
+#include "ft_calibration_node_parameters.hpp"
 
 namespace ft_tools
 {
 
-class FtCalibrationNode : public rclcpp::Node
+class FtCalibrationNode : public rclcpp_lifecycle::LifecycleNode
 {
 public:
   FtCalibrationNode();
 
   bool update_parameters();
 
+  bool update_robot_state();
+
+  void callback_new_raw_wrench(const geometry_msgs::msg::WrenchStamped & msg_raw_wrench);
+
   void add_calibration_sample(
-    const std::shared_ptr<ft_msgs::srv::AddCalibrationSample::Request> request,
-    std::shared_ptr<ft_msgs::srv::AddCalibrationSample::Response> response);
+    const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+    std::shared_ptr<std_srvs::srv::Trigger::Response> response);
 
   void get_calibration(
     const std::shared_ptr<ft_msgs::srv::GetCalibration::Request> request,
@@ -56,20 +68,47 @@ public:
     std::shared_ptr<std_srvs::srv::Trigger::Response> response);
 
 protected:
+  bool init_kinematics_monitoring();
+  bool register_services();
+
+  // Parameters management
+  std::shared_ptr<ft_calibration_node::ParamListener> parameter_handler_;
+  ft_calibration_node::Params parameters_;
+
+  // Topic names
+  std::string topic_raw_wrench_, topic_joint_state_;
+
+  /// Raw wrench measurements subscriber
+  rclcpp::Subscription<geometry_msgs::msg::WrenchStamped>::SharedPtr
+    raw_wrench_subscriber_{nullptr};
+
+  /// Joint state monitor (i.e., subscriber + utils)
+  ft_tools::JointStateMonitor robot_joint_state_monitor_;
+
+  /// Kinematics interface plugin loader
+  std::shared_ptr<pluginlib::ClassLoader<kinematics_interface::KinematicsInterface>>
+  kinematics_loader_;
+
+  /// Kinematics interface
+  std::unique_ptr<kinematics_interface::KinematicsInterface> kinematics_;
+
   // Services
-  rclcpp::Service<ft_msgs::srv::AddCalibrationSample>::SharedPtr srv_add_calibration_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr srv_add_calibration_;
   rclcpp::Service<ft_msgs::srv::GetCalibration>::SharedPtr srv_get_calibration_;
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr srv_save_calibration_;
   rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr srv_reset_;
 
   // F/T calibration utils
-  unsigned int min_nb_samples_;
   FtCalibration ft_calibration_process_;
 
   // Data
-  Eigen::Vector3d gravity_in_ref_frame_;
-  Eigen::Isometry3d sensor_frame_wrt_ref_frame_;
+  Eigen::Vector3d gravity_in_reference_frame_;
+  geometry_msgs::msg::WrenchStamped msg_raw_wrench_;
   Eigen::Matrix<double, 6, 1> raw_wrench_;
+  Eigen::Isometry3d sensor_frame_wrt_robot_base_;
+  Eigen::Matrix<double, 6, 1> sensor_twist_wrt_robot_base_;
+  Eigen::Isometry3d ref_frame_wrt_robot_base_;
+  Eigen::Isometry3d sensor_frame_wrt_ref_frame_;
 };
 
 }  // namespace ft_tools
--- ft_tools/src/ft_calibration_node.cpp
+++ ft_tools/src/ft_calibration_node.cpp
@@ -25,69 +25,152 @@ using namespace std::chrono_literals;
 namespace ft_tools
 {
 FtCalibrationNode::FtCalibrationNode()
-: Node("ft_calibration_node")
+: LifecycleNode("ft_calibration_node")
 {
-  this->declare_parameter<int>("min_nb_samples", 10);
-  update_parameters();
+  topic_raw_wrench_ = "raw_wrench";
+  topic_joint_state_ = "joint_states";
+
+  bool all_ok = true;
+  parameter_handler_ = std::make_shared<ft_calibration_node::ParamListener>(
+    this->get_node_parameters_interface());
+  parameters_ = parameter_handler_->get_params();
+
+  all_ok &= update_parameters();
   ft_calibration_process_.reset();
-  // Register services
-  srv_add_calibration_ = this->create_service<ft_msgs::srv::AddCalibrationSample>(
-    "add_calibration_sample",
-    std::bind(&FtCalibrationNode::add_calibration_sample, this, _1, _2)
+  all_ok &= init_kinematics_monitoring();
+  all_ok &= register_services();
+
+  if (!all_ok) {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Error while initializing 'ft_calibration_node'!"
+    );
+    // TODO(tpoignonec): exit node (or use lifecycle management properly...)
+  }
+}
+
+bool FtCalibrationNode::update_robot_state()
+{
+  // Read follower joint state and update kinematics
+  if (!robot_joint_state_monitor_.update() || !robot_joint_state_monitor_.check_timeout()) {
+    auto clock = this->get_clock();
+    RCLCPP_WARN_THROTTLE(this->get_logger(), *clock, 1000, "Failed to update robot joint state!");
+    return false;
+  }
+
+  bool success = true;   // return flag
+
+  const Eigen::VectorXd joint_pos = Eigen::Map<const Eigen::VectorXd>(
+    robot_joint_state_monitor_.get_joint_positions().data(),
+    robot_joint_state_monitor_.get_joint_positions().size()
   );
-  srv_get_calibration_ = this->create_service<ft_msgs::srv::GetCalibration>(
-    "get_calibration",
-    std::bind(&FtCalibrationNode::get_calibration, this, _1, _2)
+  const Eigen::VectorXd joint_vel = Eigen::Map<const Eigen::VectorXd>(
+    robot_joint_state_monitor_.get_joint_velocities().data(),
+    robot_joint_state_monitor_.get_joint_velocities().size()
   );
-  srv_save_calibration_ = this->create_service<std_srvs::srv::Trigger>(
-    "save_calibration",
-    std::bind(&FtCalibrationNode::save_calibration, this, _1, _2)
+
+  // Update sensor pose and twist w.r.t. robot base
+  success &= kinematics_->calculate_link_transform(
+    joint_pos,
+    parameters_.calibration.sensor_frame.id,
+    sensor_frame_wrt_robot_base_
   );
-  srv_reset_ = this->create_service<std_srvs::srv::Trigger>(
-    "reset",
-    std::bind(&FtCalibrationNode::reset, this, _1, _2)
+  success = kinematics_->convert_joint_deltas_to_cartesian_deltas(
+    joint_pos,
+    joint_vel,
+    parameters_.calibration.sensor_frame.id,
+    sensor_twist_wrt_robot_base_
   );
+
+  // Update reference pose w.r.t. robot base
+  success &= kinematics_->calculate_link_transform(
+    joint_pos,
+    parameters_.calibration.reference_frame.id,
+    ref_frame_wrt_robot_base_
+  );
+
+  // Update sensor pose w.r.t. robot base
+  if (success) {
+    sensor_frame_wrt_ref_frame_ = \
+      sensor_frame_wrt_robot_base_ * ref_frame_wrt_robot_base_.inverse();
+  }
+
+  return success;
+}
+
+void FtCalibrationNode::callback_new_raw_wrench(
+  const geometry_msgs::msg::WrenchStamped & msg_raw_wrench)
+{
+  msg_raw_wrench_ = msg_raw_wrench;
 }
 
 bool FtCalibrationNode::update_parameters()
 {
-  min_nb_samples_ = this->get_parameter("min_nb_samples").as_int();
-  return min_nb_samples_ >= 5;
+  bool all_ok = true;
+  // Refresh parameters
+  if (parameter_handler_->is_old(parameters_)) {
+    parameters_ = parameter_handler_->get_params();
+  }
+  // Retrieve gravity
+  auto vector_gravity = parameters_.calibration.gravity_in_reference_frame;
+  if (vector_gravity.size() != 3) {
+    all_ok &= false;
+  } else {
+    gravity_in_reference_frame_[0] = vector_gravity[0];
+    gravity_in_reference_frame_[1] = vector_gravity[1];
+    gravity_in_reference_frame_[2] = vector_gravity[2];
+  }
+  return true;
 }
 
 void FtCalibrationNode::add_calibration_sample(
-  const std::shared_ptr<ft_msgs::srv::AddCalibrationSample::Request> request,
-  std::shared_ptr<ft_msgs::srv::AddCalibrationSample::Response> response)
+  const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+  std::shared_ptr<std_srvs::srv::Trigger::Response> response)
 {
+  (void)request;
   if (!update_parameters()) {
     response->success = false;
     response->message = "Invalid parameters!";
     return;
   }
-  // Copy wrench
-  raw_wrench_[0] = request->raw_wrench.force.x;
-  raw_wrench_[1] = request->raw_wrench.force.y;
-  raw_wrench_[2] = request->raw_wrench.force.z;
-  raw_wrench_[3] = request->raw_wrench.torque.x;
-  raw_wrench_[4] = request->raw_wrench.torque.y;
-  raw_wrench_[5] = request->raw_wrench.torque.z;
-  // Copy sensor pose
-  tf2::fromMsg(request->sensor_frame_wrt_ref_frame, sensor_frame_wrt_ref_frame_);
-  // Copy gravity
-  gravity_in_ref_frame_[0] = request->gravity_in_ref_frame.x;
-  gravity_in_ref_frame_[1] = request->gravity_in_ref_frame.y;
-  gravity_in_ref_frame_[2] = request->gravity_in_ref_frame.z;
-  if (gravity_in_ref_frame_.norm() < 0.1) {
+  rclcpp::Time now = this->get_clock()->now();
+
+  // Extract raw wrench
+  rclcpp::Time stamp_wrench = msg_raw_wrench_.header.stamp;
+  double wrench_staleness = now.seconds() - stamp_wrench.seconds();
+  if (wrench_staleness > 1) {
     response->success = false;
-    response->message = "Srv field 'gravity_in_ref_frame' not set!";
+    response->message = "Stale wrench measurement!";
     return;
   }
+  if (msg_raw_wrench_.header.frame_id != parameters_.calibration.sensor_frame.id) {
+    // TODO(tpoignonec): if in ref frame, do the conversion...
+    response->success = false;
+    response->message = "Wrench should be in sensor frame!";
+    return;
+  }
+  raw_wrench_[0] = msg_raw_wrench_.wrench.force.x;
+  raw_wrench_[1] = msg_raw_wrench_.wrench.force.y;
+  raw_wrench_[2] = msg_raw_wrench_.wrench.force.z;
+  raw_wrench_[3] = msg_raw_wrench_.wrench.torque.x;
+  raw_wrench_[4] = msg_raw_wrench_.wrench.torque.y;
+  raw_wrench_[5] = msg_raw_wrench_.wrench.torque.z;
+
+  // TODO(tpoignonec): check kinematics timeout!
+  bool kinematics_ok = update_robot_state();
+  if (!kinematics_ok) {
+    auto clock = this->get_clock();
+    RCLCPP_WARN_THROTTLE(this->get_logger(), *clock, 1000, "Failed to update robot kinematics!");
+    response->success = false;
+    response->message = "Failed to retrieve robot state or kinematics!";
+    return;
+  }
+
   // Add sample
   Eigen::Vector3d gravity_in_sensor_frame =
-    sensor_frame_wrt_ref_frame_.rotation().transpose() * gravity_in_ref_frame_;
+    sensor_frame_wrt_ref_frame_.rotation().transpose() * gravity_in_reference_frame_;
   ft_calibration_process_.add_measurement(gravity_in_sensor_frame, raw_wrench_);
   // Return
-  response->nb_sample = ft_calibration_process_.get_number_measurements();
   response->success = true;
 }
 
@@ -96,7 +179,7 @@ void FtCalibrationNode::get_calibration(
   std::shared_ptr<ft_msgs::srv::GetCalibration::Response> response)
 {
   (void)request;
-  if (ft_calibration_process_.get_number_measurements() < min_nb_samples_) {
+  if (ft_calibration_process_.get_number_measurements() < parameters_.calibration.min_nb_samples) {
     response->success = false;
     response->message = "Not enough measurements!";
     return;
@@ -131,6 +214,90 @@ void FtCalibrationNode::reset(
     return;
   }
   response->success = true;
+}
+
+bool FtCalibrationNode::init_kinematics_monitoring()
+{
+  // Make sure it has not been initialized before
+  if (kinematics_loader_ || kinematics_ || raw_wrench_subscriber_) {
+    return false;
+  }
+  bool all_ok = true;
+  // Load the differential IK plugin
+  if (!parameters_.kinematics.plugin_name.empty()) {
+    try {
+      kinematics_loader_ =
+        std::make_shared<pluginlib::ClassLoader<kinematics_interface::KinematicsInterface>>(
+        parameters_.kinematics.plugin_package,
+        "kinematics_interface::KinematicsInterface");
+      kinematics_ = std::unique_ptr<kinematics_interface::KinematicsInterface>(
+        kinematics_loader_->createUnmanagedInstance(parameters_.kinematics.plugin_name));
+      if (!kinematics_->initialize(
+          this->get_node_parameters_interface(), parameters_.kinematics.tip))
+      {
+        all_ok &= false;
+      }
+    } catch (pluginlib::PluginlibException & ex) {
+      RCLCPP_ERROR(
+        this->get_logger(),
+        "Exception while loading the IK plugin '%s': '%s'",
+        parameters_.kinematics.plugin_name.c_str(), ex.what()
+      );
+      all_ok &= false;
+    }
+  } else {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "A differential IK plugin name was not specified in the config file.");
+    all_ok &= false;
+  }
+
+  // Monitor follower joints state
+  RCLCPP_INFO(this->get_logger(), "'topic joint state' : %s", topic_joint_state_.c_str());
+  all_ok &= robot_joint_state_monitor_.init(
+    std::shared_ptr<rclcpp_lifecycle::LifecycleNode>(this),
+    topic_joint_state_);
+
+  if (all_ok) {
+    // Create raw wrench subscriber
+    raw_wrench_subscriber_ =
+      this->create_subscription<geometry_msgs::msg::WrenchStamped>(
+      topic_raw_wrench_,
+      2,
+      std::bind(&FtCalibrationNode::callback_new_raw_wrench, this, _1));
+  } else {
+    RCLCPP_ERROR(
+      this->get_logger(),
+      "Failed to init the wrench estimator! Make sure the config and urdf files are correct.");
+  }
+  return all_ok;
+}
+
+bool FtCalibrationNode::register_services()
+{
+  // Make sure it has not been initialized before
+  if (srv_add_calibration_ || srv_get_calibration_ || srv_save_calibration_ || srv_reset_) {
+    return false;
+  }
+  bool all_ok = true;
+  // Register services
+  srv_add_calibration_ = this->create_service<std_srvs::srv::Trigger>(
+    "add_calibration_sample",
+    std::bind(&FtCalibrationNode::add_calibration_sample, this, _1, _2)
+  );
+  srv_get_calibration_ = this->create_service<ft_msgs::srv::GetCalibration>(
+    "get_calibration",
+    std::bind(&FtCalibrationNode::get_calibration, this, _1, _2)
+  );
+  srv_save_calibration_ = this->create_service<std_srvs::srv::Trigger>(
+    "save_calibration",
+    std::bind(&FtCalibrationNode::save_calibration, this, _1, _2)
+  );
+  srv_reset_ = this->create_service<std_srvs::srv::Trigger>(
+    "reset",
+    std::bind(&FtCalibrationNode::reset, this, _1, _2)
+  );
+  return all_ok;
 }
 
 }  // namespace ft_tools
