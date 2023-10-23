f5a1dfbe62600b794acdde57028600dc015f89fd
Thibault Poignonec <thibault.poignonec@gmail.com>
2023-10-23 17:15:37 +0200

    add empty launch controllers file

---
+++ ft_tools_examples/launch/launch_robot_controllers.launch.py
@@ -0,0 +1,35 @@
+# Copyright 2023 ICube Laboratory, University of Strasbourg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+# from launch.conditions import IfCondition, UnlessCondition
+# from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Declare arguments
+    declared_arguments = []
+
+    # Get URDF via xacro
+
+    # Launch iiwa robot with Moveit2 planning enabled
+
+    nodes = []
+
+    return LaunchDescription(declared_arguments + nodes)
